9c68ba3c989f994ee3c3173b06a66887f0b6cedc
Andy Wong <awong@pdx.edu>
2021-02-15 19:33:26 -0800

    Following "Handling Errors & Improving the Project Setup"
    Added automatic server restarting (after source changes) w/ nodemon,
    logging w/ morgan, and error handling in the app.js file.

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan'); /* Middleware used for logging. */
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
+/* Routes which should handle requests. */
 app.use('/products', productRoutes); /* Forwards all server requests to the first arg.
                                         First argument is a "filter" that looks for 
                                         requests starting w/ the given path, and the
                                         second arg. is a handler. Requests will be
                                         forwarded to the file given in productRoutes.*/
 
-
 app.use('/orders', orderRoutes); 
+
+/*Handle errors (any requests that make it to this point). */
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error); /*Forwards the error request. */
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500); /*500 for all other types of errors. */
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 module.exports = app;
