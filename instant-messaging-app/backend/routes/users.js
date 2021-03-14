d6d53275f1b64dfe00090ede362414326e3ac322
Andy Wong <awong@pdx.edu>
2021-03-13 21:52:13 -0800

    Implemented password authentication w/ frontend & backend
    Followed similar format to v1 of the IMA and added a password
    requirement to creating new users. Also included the regex check for
    usernames, so that new users must enter properly formatted email
    addresses. The frontend reflects the change in the 'Create User' tab and
    also checks for and creates an error message for duplicate users.

--- instant-messaging-app/backend/routes/users.js
+++ instant-messaging-app/backend/routes/users.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 let User = require('../models/user.model');
+const mongoose = require('mongoose');
 
 router.route('/').get((req, res) => {
   User.find()
@@ -8,13 +10,35 @@ router.route('/').get((req, res) => {
 });
 
 router.route('/add').post((req, res) => {
-  const username = req.body.username;
+  User.find({ username: req.body.username })
+    //.exec()
+    .then(user => {
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: 'Account with username already exists'
+        });
+      }
+      else {
+    //const username = req.body.username;
+    /*const password =*/ bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+          return res.status(500).json({
+            error: err
+          });
+        }
+        else {
+          const newUser = new User({
+            username: req.body.username,
+            password: hash
+          });
 
-  const newUser = new User({username});
-
-  newUser.save()
-    .then(() => res.json('User added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+          newUser.save()
+            .then(() => res.json('User added!'))
+            .catch(err => res.status(400).json('Error: ' + err));
+        }
+      });
+      }
+    })
 });
 
 module.exports = router;
--- instant-messaging-app/src/components/create-user.component.js
+++ instant-messaging-app/src/components/create-user.component.js
@@ -5,10 +5,13 @@ export default class CreateUser extends Component {
     constructor(props) {
         super(props); 
         this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
         this.onSubmit = this.onSubmit.bind(this); 
         
         this.state = {
-            username: ''
+            username: '',
+            password: '',
+            errorMessage: ''
         };
     }
 
@@ -18,19 +21,38 @@ export default class CreateUser extends Component {
         });
     }
 
+    onChangePassword(e) {
+        this.setState({
+            password: e.target.value
+        });
+    }
+
     onSubmit(e) {
         e.preventDefault();
+        this.setState({
+            errorMessage: ''
+        });
         const newUser = {
             username: this.state.username,
+            password: this.state.password
         };
 
         console.log(newUser);
 
         axios.post('http://localhost:5000/users/add', newUser)
-        .then(res => console.log(res.data));
+        .then(res => console.log(res.data))
+        .catch(err =>  {
+            if(err.response.status === 409){
+                this.setState({errorMessage: "User already exists"})
+            }
+            else{
+                this.setState({errorMessage: err.message});
+            }
+        });
 
         this.setState({
-            username: ''
+            username: '',
+            password: ''
         })
     }
 
@@ -47,11 +69,23 @@ export default class CreateUser extends Component {
                             value={this.state.username}
                             onChange={this.onChangeUsername}
                         />
+                        <label>Password: </label>
+                        <input type="password"
+                            required
+                            className="form-control"
+                            value={this.state.password}
+                            onChange={this.onChangePassword}
+                        />                    
                     </div>
                     <div className="form-group">
                         <input type="submit" value="Create User" className="btn btn-primary" />
                     </div>
                 </form>
+                {this.state.errorMessage.length > 0 &&
+                    <h2>
+                        Sorry, {this.state.errorMessage}.
+                    </h2>
+                };
             </div>
         );
     }
